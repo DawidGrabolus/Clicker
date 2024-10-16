fe7099c9d2b77921534d9792f909b785d3c57974
unknown <dawidgrabolus@gmail.com>
2024-10-16 15:26:17 +0200

    First commit

---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System.ComponentModel;
+using System.Windows.Input;
+using Microsoft.Maui.Controls;
+
+namespace Clicker.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private int points;
+        public int Points
+        {
+            get => points;
+            set
+            {
+                if (points != value)
+                {
+                    points = value;
+                    OnPropertyChanged(nameof(Points));
+                }
+            }
+        }
+
+        public ICommand IncreasePointsCommand { get; }
+        public ICommand DecreasePointsCommand { get; }
+
+        public MainViewModel()
+        {
+            IncreasePointsCommand = new Command(IncreasePoints);
+            DecreasePointsCommand = new Command(DecreasePoints);
+            Points = 0;
+        }
+
+        private void IncreasePoints()
+        {
+            Points++;
+        }
+
+        private void DecreasePoints()
+        {
+            if (Points > 0)
+            {
+                Points--;
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ Views/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Clicker.ViewModels;
+namespace Clicker;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+        BindingContext = new MainViewModel();
+
+    }
+}
